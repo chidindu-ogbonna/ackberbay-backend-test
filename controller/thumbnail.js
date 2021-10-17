a0f486f4719a4ee2dc500df234e67fb8f3cdcbf8
bones <promise.bones@gmail.com>
2021-10-17 15:15:51 +0100

    refactor: Send the thumbnail as the response

--- controller/thumbnail.js
+++ controller/thumbnail.js
@@ -11,7 +11,8 @@ const { validationResponse } = require("../utils");
 const pipeline = promisify(stream.pipeline);
 
 /**
- * Thumbnail generator
+ * Generate thumbnail from a public image url.
+ * Download the image, resize to 50x50 pixels, and return the resulting thumbnail.
  * @param {import('express').Request} request
  * @param {import('express').Response} response
  */
@@ -27,21 +28,33 @@ const thumbnail = async (request, response) => {
 
     const filename = `${randomString}.${ext}`;
     const fileLocation = `/tmp/${filename}`;
+    const thumbnailLocation = `/tmp/${randomString}_thumbnail.${ext}`;
+
     await pipeline(got.stream(image_url), fs.createWriteStream(fileLocation));
 
     await sharp(fileLocation)
       .resize(50, 50)
       .jpeg({ mozjpeg: true })
-      .toFile(`/tmp/${randomString}_thumbnail.${ext}`);
+      .toFile(thumbnailLocation);
 
     // TODO: Save in cloudinary
 
-    return response.status(StatusCodes.OK).json({
-      data: {
-        filename: `${randomString}_thumbnail.${ext}`,
-        user: request["user"],
-      },
-    });
+    // var fileName = req.params.name;
+    return response.sendFile(thumbnailLocation);
+
+    // response.sendFile(fileName, options, (err) => {
+    //   if (err) {
+    //     next(err);
+    //   } else {
+    //     console.log("Sent:", fileName);
+    //   }
+    // });
+
+    // return response.status(StatusCodes.OK).json({
+    //   data: {
+    //     filename: `${randomString}_thumbnail.${ext}`,
+    //   },
+    // });
   } catch (error) {
     if (error.name === "ValidationError") {
       return response.status(StatusCodes.BAD_REQUEST).json({
