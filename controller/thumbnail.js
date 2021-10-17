03573e230f3e371993f99d59fa665f3e12c6c6ff
bones <promise.bones@gmail.com>
2021-10-17 14:46:24 +0100

    feat: Implement thumbnail endpoint

--- controller/thumbnail.js
+++ controller/thumbnail.js
@@ -1,7 +1,14 @@
 /* global require module */
+const fs = require("fs");
+const sharp = require("sharp");
+const got = require("got");
+const { promisify } = require("util");
+const stream = require("stream");
 const { body } = require("express-validator");
 const { StatusCodes } = require("http-status-codes");
 const { validationResponse } = require("../utils");
+
+const pipeline = promisify(stream.pipeline);
 
 /**
  * Thumbnail generator
@@ -10,22 +17,53 @@ const { validationResponse } = require("../utils");
  */
 const thumbnail = async (request, response) => {
   try {
-    // validationResponse(request, response);
-    console.log("Here: ", request.path);
-    console.log("Request: ", request.body);
+    validationResponse(request);
 
-    return response
-      .status(StatusCodes.OK)
-      .json({ data: { user: request["user"], message: "succss" } });
-  } catch (err) {
+    const { image_url } = request.body;
+
+    const split = image_url.split(".");
+    const ext = image_url.split(".")[split.length - 1];
+    const randomString = (Math.random() + 1).toString(36).substring(2);
+
+    const filename = `${randomString}.${ext}`;
+    const fileLocation = `/tmp/${filename}`;
+    await pipeline(got.stream(image_url), fs.createWriteStream(fileLocation));
+
+    await sharp(fileLocation)
+      .resize(50, 50)
+      .jpeg({ mozjpeg: true })
+      .toFile(`/tmp/${randomString}_thumbnail.${ext}`);
+
+    // TODO: Save in cloudinary
+
+    return response.status(StatusCodes.OK).json({
+      data: {
+        filename: `${randomString}_thumbnail.${ext}`,
+        user: request["user"],
+      },
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return response.status(StatusCodes.BAD_REQUEST).json({
+        error: {
+          name: error.name,
+          message: error.message,
+          validation_errors: error.validation_errors,
+        },
+      });
+    }
+
     return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       error: {
-        name: err.name,
-        message: err.message,
+        name: error.name,
+        message: error.message,
       },
     });
   }
 };
 
 module.exports = thumbnail;
-module.exports.validators = [body("file").isString()];
+module.exports.validators = [
+  body("image_url").isString().withMessage("image url must be a string"),
+  body("image_url").isURL().withMessage("image url must be an actual url"),
+];
